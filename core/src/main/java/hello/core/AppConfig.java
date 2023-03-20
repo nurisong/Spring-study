d603854175b1d7ef8e5cfaaa61c8dbfdfa3d64cc
nurisong <noooori0827@gmail.com>
2023-03-20 15:22:21 +0900

    AppConfig 리팩토링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,11 +14,24 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자에 Memory~~가
+        // return new MemberServiceImpl(new MemoryMemberRepository()); // 문제점: 역할에 따른 구현이 한눈에 드러나지
+        return new MemberServiceImpl(memberRepository()); // c+a+M
         // 생성자 주입
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private static MemberRepository memberRepository() { // 반환형은 인터페이스
+        return new MemoryMemberRepository();
     }
+
+    public OrderService orderService() {
+        // return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+    // 리펙토링 후 -> 중복제거, "역할"과 "구현"클래스가 한눈에 다 드러남 -> 애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악 가능
+    // 사용 영역과 구성 영역으로 나눠짐
 }
